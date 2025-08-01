02b0145a5b9719a1a55c5449e90d5e52b482aba2
CodingLikeAMonkey <aaron.michael.friedrich@gmail.com>
2025-08-01 22:58:48 +0200

    Added large files via git LFS

--- 07_Scripts/03_Systems/CharacterSystems/CharacterLastPositionSystem.cs
+++ 07_Scripts/03_Systems/CharacterSystems/CharacterLastPositionSystem.cs
@@ -47,7 +47,6 @@ public static class CharacterLastPositonSystem
                             entity.Set(timer);
                         }
                     }
-
                 }
             });
     }
