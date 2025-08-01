cc56e4939e9d0dc9de9f1a2d65b8e719ed80d9e2
CodingLikeAMonkey <aaron.michael.friedrich@gmail.com>
2025-08-02 01:48:00 +0200

    Improving ariborne andground controls

--- 07_Scripts/02_Components/CharacterComponents/CharacterMovementStatsComponent.cs
+++ 07_Scripts/02_Components/CharacterComponents/CharacterMovementStatsComponent.cs
@@ -10,18 +10,27 @@ public struct CharacterMovementStatsComponent
     public float Friction;
     public float TurnSpeed;
     public float JumpImpulse;
+    public float AirAcceleration;
+    public float AirTurnSpeed;
     public float CurrentSpeed;
+    public float CurrentTurnSpeed;
+    public float CurrentAcceleration;
 
     public CharacterMovementStatsComponent()
     {
         WalkThreshold = 0.7f;
         TapThreshold = 0.06f;
-        Speed = 5.0f;
-        WalkSpeed = 2.0f;
-        Acceleration = 35.0f;
-        Friction = 50.0f;
+        Speed = 3.0f;
+        WalkSpeed = 1.2f;
+        Acceleration = 25.0f;
+        Friction = 35.0f;
         TurnSpeed = 10.0f;
         JumpImpulse = 5.0f;
+        AirAcceleration = 0.05f;
+        AirTurnSpeed = 0.1f;
         CurrentSpeed = 0.0f;
+        CurrentTurnSpeed = 0.0f;
+        CurrentAcceleration = 0.0f;
+
     }
 }
